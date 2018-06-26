074d64606bc777a653fb2e1c6169a903ecca3d2d
bell-wu <wu.bell0322@gmail.com>
2018-06-26 13:03:43 -0700

    removed an icon

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(function () {$('[data-toggle="popover"]').popover()});
+
+$('.popover-dismiss').popover({trigger: 'focus'});
+
+$(document).on('click', function (e) {
+    $('[data-toggle="popover"],[data-original-title]').each(function () {
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {                
+            (($(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false  // fix for BS 3.3.6
+        }
+
+    });
+});
